251c9c23edf8e17297690ac82888ab8c060d3f2b
Ngo Dinh Nhat Hoang <ngo.dinh.nhat.hoang@framgia.com>
2019-01-15 16:32:30 +0700

    Commit up to part 9

--- Gemfile
+++ Gemfile
@@ -16,6 +16,9 @@ gem 'bootstrap', '~> 4.2.1'
 gem 'jquery-rails'
 gem 'friendly_id', '~> 5.2.4'
 gem 'devise'
+gem 'annotate'
+gem 'will_paginate', '~> 3.1.0'
+gem 'acts-as-taggable-on', '~> 6.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: authors
+#
+#  id                     :integer          not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  name                   :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_authors_on_email                 (email) UNIQUE
+#  index_authors_on_reset_password_token  (reset_password_token) UNIQUE
+#
+
 class Author < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :registerable,:timeoutable, :trackable and :omniauthable
